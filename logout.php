8a72b5810090bed277e5ac48228e94fddfab502a
Zeryyern <zeryyernawwerl@student.uns.ac.id>
2025-06-24 09:27:14 -0700

    added project icon across all files

--- logout.php
+++ logout.php
@@ -12,6 +12,7 @@ if (!isset($_SESSION['user_id'])) {
 <head>
     <meta charset="UTF-8" />
     <title>Logout Confirmation</title>
+    <link rel="icon" href="assets/images/dietIcon.png" />
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0/dist/css/bootstrap.min.css" rel="stylesheet" />
 </head>
 
